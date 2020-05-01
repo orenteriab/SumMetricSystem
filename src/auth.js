5ea3f4dacdec7a83629566194e2be9d2962f9a33
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-04-30 23:24:47 -0600

    simple authentication handler

---
+++ src/auth.js
@@ -0,0 +1,19 @@
+import { AUTH_KEY } from './config';
+
+/**
+ * Simulates a simple authorization middleware.
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ * @typedef {import('express').NextFunction} NextFunction
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+const authMiddleware = (req, res, next) => {
+  if (req.headers.authorization !== AUTH_KEY) {
+    return res.status(401).json({ error: 'You are not authorized to perform this action.' });
+  }
+  next();
+};
+
+export default authMiddleware;
