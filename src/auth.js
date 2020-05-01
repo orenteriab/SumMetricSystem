4d4c579318fa9ef8fd3443b48587f67a6d423000
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-01 01:05:45 -0600

    making it more compact

--- src/auth.js
+++ src/auth.js
@@ -9,11 +9,9 @@ import { AUTH_KEY } from './config';
  * @param {Response} res
  * @param {NextFunction} next
  */
-const authMiddleware = (req, res, next) => {
+export default function authMiddleware (req, res, next) {
   if (req.headers.authorization !== AUTH_KEY) {
     return res.status(401).json({ error: 'You are not authorized to perform this action.' });
   }
   next();
 };
-
-export default authMiddleware;
