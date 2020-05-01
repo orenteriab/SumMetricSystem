543d9cdceb7ce1f2974d0d487a0d99b5611dbeb9
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-01 00:07:30 -0600

    Added some type hints, and improved handling of exceptions and response

--- src/index.js
+++ src/index.js
@@ -10,33 +10,54 @@ app.use(authMiddleware, bodyParser.json());
 
 app.post('/metric/:key', (req, res) => {
   const key = req.params.key;
+  /**
+   * @type {number|string}
+   */
   const value = req.body.value;
+  /**
+   * @type {number}
+   */
+  let status;
+  /**
+   * @type {({}|{error: string})}
+   */
+  let responseBody;
 
   try {
     addMetric(key, value);
+    [status, responseBody] = [200, {}];
   } catch (err) {
-    console.error(`[index][/metric/${key}]: Error encountered when adding metric.`, err);
-
     if (err instanceof TypeError) {
-      return res.status(422).json({ error: err.message });
+      [status, responseBody] = [422, { error: err.message }];
+    } else {
+      [status, responseBody] = [500, { error: 'An error occurred on the server. Please try again.' }];
+      console.error(`[index][/metric/${key}]: Error encountered when adding metric.`, err);
     }
-
-    return res.status(500).send({ error: 'An error occurred on the server. Please try again.' });
   }
 
-  res.json({});
+  res.status(status).json(responseBody);
 });
 
 app.get('/metric/:key/sum', (req, res) => {
   const key = req.params.key;
+  /**
+   * @type {number}
+   */
+  let status;
+  /**
+   * @type {({value: number} | {error: string})}
+   */
+  let responseBody;
 
   try {
     const value = sumMetricValues(key);
-    res.json({ value });
+    [status, responseBody] = [200, { value }];
   } catch (err) {
     console.error(`[index][/metric/${key}/sum]: Error encountered when adding metric.`, err);
-    res.status(500).send({ error: 'An error occurred on the server. Please try again.' });
+    [status, responseBody] = [500, { error: 'An error occurred on the server. Please try again.' }];
   }
+
+  res.status(status).json(responseBody);
 });
 
 app.listen(PORT, () => {
