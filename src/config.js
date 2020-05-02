748058228301cc95e0fb6bb35d767c9f52fb1ba9
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-01 23:32:21 -0600

    Making sure we have expire time and port as `numbers`, making the app aware of the environment that is running

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ import dotEnv from 'dotenv';
 
 dotEnv.config();
 
-export const PORT = process.env.port || process.env.PORT;
+export const PORT = parseInt(process.env.port || process.env.PORT);
 export const AUTH_KEY = process.env.AUTH_KEY;
-export const EXPIRE_TIME = process.env.EXPIRE_TIME;
+export const EXPIRE_TIME = parseInt(process.env.EXPIRE_TIME);
+export const NODE_ENV = process.env.NODE_ENV;
