0311738e16dbf978b6b71a2f311762d9a0f43236
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-01 21:54:18 -0600

    For AWS

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import dotEnv from 'dotenv';
 
 dotEnv.config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.port || process.env.PORT;
 export const AUTH_KEY = process.env.AUTH_KEY;
 export const EXPIRE_TIME = process.env.EXPIRE_TIME;
